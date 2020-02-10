a1bc4d2476f7c046940b833ad3a7de5108bab8a4
Rellyson Silva <rellysonsilva@gmail.com>
2020-02-10 16:06:41 -0300

    added some new features

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
     dialect: 'mysql',
-    host: '',
-    username:'',
-    password: '',
-    database: '',
+    host: process.env.HOST,
+    username:process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
     define: {
         timestamps: true,
         underscored: true,
