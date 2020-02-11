0d1ffba8d266d8c4afe1100fe2e50b5d20281735
Rellyson Silva <rellysonsilva@gmail.com>
2020-02-10 23:08:38 -0300

    env.port defined

--- src/server.js
+++ src/server.js
@@ -6,6 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000), () => {
-    console.log("Server running on port 3000...");
+app.listen(process.env.PORT), () => {
+    console.log(`Server running on port +${process.env.PORT} `);
 };
