6e9be5fdf9a3244e41631744d94f3ceb6303d470
Rellyson Silva <rellysonsilva@gmail.com>
2020-02-14 00:03:20 -0300

    cors enabled

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 
 app.listen(process.env.PORT), () => {
     console.log(`Server running on port +${process.env.PORT} `);
